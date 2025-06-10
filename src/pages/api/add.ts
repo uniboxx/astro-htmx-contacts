619e78558e6696a6e7d51dc6a960da40d73dabdc
unibox <unibox@duck.com>
2025-06-10 15:40:22 +0200

    some small edit

--- src/pages/api/add.ts
+++ src/pages/api/add.ts
@@ -2,19 +2,22 @@ import type { APIContext } from 'astro';
 import Contatti, { type Contact } from '@/data/contacts';
 
 export async function POST({ request }: APIContext) {
-  const formData = await request.formData();
-  const contact = Object.fromEntries(formData) as unknown as Contact;
-  console.log(contact);
+  try {
+    const formData = await request.formData();
+    const contact = Object.fromEntries(formData) as unknown as Contact;
 
-  if (!contact.email.toString().includes('@')) {
-    return new Response(null, { status: 401, statusText: 'Email not valid' });
+    if (!contact.email.toString().includes('@')) {
+      return new Response(null, { status: 401, statusText: 'Email not valid' });
+    }
+    Contatti.create(contact);
+    return new Response(null, {
+      status: 204,
+      statusText: 'Contact added successfully',
+      headers: {
+        'HX-Location': '/contacts',
+      },
+    });
+  } catch (error: any) {
+    console.log(error.message);
   }
-  Contatti.create(contact);
-  return new Response(null, {
-    status: 204,
-    statusText: 'No Content',
-    headers: {
-      'HX-Location': '/contacts',
-    },
-  });
 }
