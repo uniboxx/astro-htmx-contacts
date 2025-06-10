1716e0d88109f6c9f1094abe2f8119c5cf66c6df
unibox <unibox@duck.com>
2025-06-10 12:26:21 +0200

    fix add functionality

--- src/pages/api/add.ts
+++ src/pages/api/add.ts
@@ -1,7 +1,7 @@
 import type { APIContext } from 'astro';
 import Contatti, { type Contact } from '@/data/contacts';
 
-export async function POST({ request, redirect }: APIContext) {
+export async function POST({ request }: APIContext) {
   const formData = await request.formData();
   const contact = Object.fromEntries(formData) as unknown as Contact;
   console.log(contact);
@@ -10,5 +10,11 @@ export async function POST({ request, redirect }: APIContext) {
     return new Response(null, { status: 401, statusText: 'Email not valid' });
   }
   Contatti.create(contact);
-  return redirect('/contacts');
+  return new Response(null, {
+    status: 204,
+    statusText: 'No Content',
+    headers: {
+      'HX-Location': '/contacts',
+    },
+  });
 }
